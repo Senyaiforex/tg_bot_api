8ed333289c26ffc89a15b2160eb94e9c22f84005
ars <iiforex@mail.ru>
2024-10-09 00:47:42 +0300

    change workers = 2
    Took 4 minutes

--- app/main.py
+++ app/main.py
@@ -441,7 +441,7 @@ async def clear_cache():
 
 
 def main():
-    config = uvicorn.Config(app, host="0.0.0.0", port=8000, workers=8, log_level="info")
+    config = uvicorn.Config(app, host="0.0.0.0", port=8000, workers=2, log_level="info")
     server = uvicorn.Server(config)
     try:
         server.run()
