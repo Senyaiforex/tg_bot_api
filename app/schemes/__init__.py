5d8bfafc8751d66fbcda89b65a48246841fa615d
ars <iiforex@mail.ru>
2024-12-04 21:04:04 +0300

    fix schemes
    Took 30 minutes

--- app/schemes/__init__.py
+++ app/schemes/__init__.py
@@ -1,5 +1,6 @@
 from .users import BaseUser, UserIn, UserOut, DeleteUser, Friend, HistoryTransactionOut, UserTopOut
-from .specifications import ChangeSale, ChangeCoins, ChangeLevel, ChangePharmd, ChangePurchase, ChangeCountFriends, ChangeSpinners
+from .specifications import (ChangeSale, ChangeCoins, ChangeLevel, ChangePharmd, ChangePurchase, ChangeCountFriends,
+                             ChangeSpinners, AddTransaction, HistoryChangeTransactionOut)
 from .tasks import TaskOut, CategoriesOut, TaskByUser
 from .posts import *
 from .pull import PullOut
