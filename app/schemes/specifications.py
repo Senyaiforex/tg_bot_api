f6a75526eabab33b2c4afbcdbe1e7539f8b448c7
ars <iiforex@mail.ru>
2024-12-05 13:32:54 +0300

    add payment stars
    Took 1 minute

--- app/schemes/specifications.py
+++ app/schemes/specifications.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, Field
+from datetime import datetime
+
+from pydantic import BaseModel, Field, field_validator
 
 
 class ChangeCoins(BaseModel):
@@ -6,9 +8,11 @@ class ChangeCoins(BaseModel):
     description: str = Field(description='Описание транзакции')
     add: bool = Field(description='Добавить или вычесть(True or False)')
 
+
 class ChangeSpinners(BaseModel):
     amount: int = Field(gt=0, description='Количество спиннеров')
     add: bool = Field(description='Добавить или вычесть(True or False)')
+
 
 class ChangePharmd(BaseModel):
     amount: int = Field(gt=0, description='Количество фарма')
@@ -35,3 +39,37 @@ class ChangeCountFriends(BaseModel):
     id_telegram: int
     amount: int
     add: bool
+
+
+class AddTransaction(BaseModel):
+    from_сurrency: str
+    from_amount: int
+    to_currency: str
+    to_amount: int
+
+
+class HistoryChangeTransactionOut(BaseModel):
+    id: int = Field(..., description='ID транзакции')
+    transaction_date: datetime | str = Field(..., description='Дата транзакции')
+    from_сurrency: str
+    from_amount: int
+    to_currency: str
+    to_amount: int
+
+    @field_validator('transaction_date', mode='before')
+    def format_transaction_date(cls, v):
+        if isinstance(v, datetime):
+            return v.strftime('%d-%m-%Y %H:%M')
+        elif isinstance(v, str):
+            try:
+                parsed_date = datetime.strptime(v, '%Y-%m-%d %H:%M:%S')
+                return parsed_date.strftime('%d-%m-%Y %H:%M')
+            except ValueError:
+                pass
+        return v
+
+    class Config:
+        from_attributes = True
+
+a = {'from_сurrency': 'ton', 'from_amount': 100,
+     'to_currency': 'stars', 'to_amount': 200}
