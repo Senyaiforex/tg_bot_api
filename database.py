1cf397292670dc936094da225365ff3619a527fb
ars <iiforex@mail.ru>
2024-12-07 22:42:33 +0300

    add db port in database
    Took 37 seconds

--- database.py
+++ database.py
@@ -9,9 +9,9 @@ user = os.getenv("POSTGRES_USER") if DOCKER else 'senyaiforex'
 password = os.getenv("POSTGRES_PASSWORD") if DOCKER else 'senya2516'
 db_name = os.getenv("POSTGRES_DB") if DOCKER else 'db'
 db_host = os.getenv("POSTGRES_HOST") if DOCKER else '127.0.0.1'
-
+db_port = os.getenv("POSTGRES_PORT") if DOCKER else '5432'
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATABASE_URL = f'postgresql+asyncpg://{user}:{password}@{db_host}:5432/{db_name}'
+DATABASE_URL = f'postgresql+asyncpg://{user}:{password}@{db_host}:{db_port}/{db_name}'
 engine = create_async_engine(DATABASE_URL,
                              pool_recycle=700,
                              pool_size=100,  # производительность
