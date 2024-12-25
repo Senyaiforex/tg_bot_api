14c8f3198656943c6a35043a4c41ed803bccbcca
ars <iiforex@mail.ru>
2024-12-25 21:51:06 +0300

    add rus ranks
    Took 1 hour 21 minutes

--- app/schemes/rank.py
+++ app/schemes/rank.py
@@ -1,14 +1,34 @@
-from pydantic import BaseModel, Field, field_validator
+from pydantic import BaseModel, Field, field_validator, root_validator, model_validator
+
+dict_rank_names = {
+        "stone": "Лига исследователей",
+        "copper": "Лига Выгодных Сделок",
+        "silver": "Лига Удачных Покупок",
+        "gold": "Лига Цифровой Торговли",
+        "platinum": "Лига Лояльных покупателей",
+        "diamond": "Лига Бонусных Возможностей",
+        "sapphire": "Лига Звездных Сделок",
+        "ruby": "Лига Топ-Маркетплейсов",
+        "amethyst": "Лига Экспертов Маркетплейсов",
+        "morganite": "Лига Покупательской Славы",
+}
 
 
 class Rank(BaseModel):
     id: int = Field(..., description='ID ранга')
     rank: str = Field(default=..., description='Название ранга')
     level: int = Field(default=..., description='Уровень')
+    rank_name: str = Field(default=..., description='Название ранга на русском')
 
     @field_validator('rank', mode='before')
-    def format_transaction_date(cls, v):
+    def format_rank(cls, v):
         return v.name
+
+    @model_validator(mode='before')
+    def set_rank_name(cls, values):
+        rank = values.rank.name
+        values.rank_name = dict_rank_names[rank]
+        return values
 
     class Config:
         from_attributes = True
@@ -25,21 +45,34 @@ class RankInfoOut(BaseModel):
     rank: str = Field(default=1, description='Название ранга')
     level: int = Field(default=1, description='Уровень текущего ранга')
     conditions: list[ConditionOut]
+    rank_name: str = Field(default=..., description='Название ранга на русском')
 
     @field_validator('rank', mode='before')
-    def format_transaction_date(cls, v):
+    def format_rank(cls, v):
         return v.name
 
+    @model_validator(mode='before')
+    def set_rank_name(cls, values):
+        rank = values.rank.name
+        values.rank_name = dict_rank_names[rank]
+        return values
 
 class RankOutInfo(BaseModel):
     id: int = Field(..., description='ID ранга')
     rank: str = Field(default=..., description='Название ранга')
     level: int = Field(default=..., description='Уровень')
     required_coins: int = Field(default=..., description='Требуемое количество монет')
+    rank_name: str = Field(default=..., description='Название ранга на русском')
 
     @field_validator('rank', mode='before')
-    def format_transaction_date(cls, v):
+    def format_rank(cls, v):
         return v.name
+
+    @model_validator(mode='before')
+    def set_rank_name(cls, values):
+        rank = values.rank.name
+        values.rank_name = dict_rank_names[rank]
+        return values
 
     class Config:
         from_attributes = True
